498ca2def134b058ce3f9e1c2304ffdd9e773d88
jcmatos97 <jcmatos97@outlook.com>
2020-11-18 00:51:20 -0400

    noche 18-11-20

--- src/sendFile.ts
+++ src/sendFile.ts
@@ -1,8 +1,11 @@
 export class SendFile {
 
     private ftp: any = require("basic-ftp");
+    private path: string = __dirname.split("\\").slice(0, __dirname.split("\\").length-1).join("/");
 
     constructor() {
+        console.log("Proceso de Subida iniciado...");
+        this.uploadFile();
     }
 
     /**
@@ -11,12 +14,33 @@ export class SendFile {
     private downloadFile(): any {
         
     }
-//https://www.npmjs.com/package/basic-ftp
-//https://www.typescripttutorial.net/typescript-tutorial/typescript-getters-setters/
+
     /**
      * uploadFile
      */
-    private uploadFile(): any {
-        
+    private async uploadFile(): Promise<any> {
+        const client = new this.ftp.Client();
+        client.ftp.verbose = true
+        try {
+            await client.access({
+                host: "10.0.0.13",
+                port: "21",
+                user: "jcmaster",
+                password: "123456",
+                secure: false
+            })
+            console.log(await client.list());
+            //console.log(this.path+"/uploadFolder/archivo.txt");
+            //await client.uploadFrom(this.path+"/uploadFolder/archivo.txt", "/");
+            await client.uploadFrom("C:/Users/JC97/Desktop/UNAPEC/Cuatrimestre VII/Integracion Open Source/UploadFileFTP/uploadFolder/archivo.txt", "/");
+
+        }
+        catch(err) {
+            console.log(err)
+        }
+        client.close();   
     }
+
+    //https://www.npmjs.com/package/basic-ftp
+    //https://www.typescripttutorial.net/typescript-tutorial/typescript-getters-setters/
 }
